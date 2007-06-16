e84e9bf162eea2b5ab6533dbac176fae0aadb6eb
Christopher Wellons <mosquitopsu@gmail.com>
2007-06-16 21:54:17 +0000

    Working "library" call for encoding datpng.

--- datpng.h
+++ datpng.h
@@ -8,10 +8,18 @@
 /* Describes how data should be structured in the PNG. */
 typedef struct datpng_info
 {
+  char *filename;
   int bit_depth;
   int checksum;
+  int x_pos;
+  int y_pos;
+  int data_width;
+  int data_height;
+  int png_width;
+  int png_height;
 } datpng_info;
 
-int datpng_write(char *filename, datpng_info *dat_info, void *data);
+int datpng_write(char *filename, datpng_info *dat_info, 
+		 void *data, size_t data_size);
 
 #endif
--- encode.c
+++ encode.c
@@ -4,77 +4,73 @@
 
 #include "datpng.h"
 
-int width = 100;
 int data_size;
 
-int main()
+int datpng_write(char *filename, datpng_info *dat_info, 
+		 void *data, size_t data_size)
 {
-  png_bytep *row_pointers;
-
-  int rp_size = 64;
-  data_size = 0;
-
-  row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * rp_size);
+  int byte_depth = 1;
+  int rowbytes = dat_info->data_width * 3 * byte_depth;
   
-  size_t amt;
-  while (!feof(stdin))
-    {
-      row_pointers[data_size] = (png_bytep)malloc(width * 3);
-      amt = fread(row_pointers[data_size], 1, width * 3, stdin);
-      data_size++;
-      if (data_size > rp_size)
-	{
-	  rp_size *= 2;
-	  row_pointers = (png_bytep *)
-	    realloc(row_pointers, sizeof(png_bytep) * rp_size);
-	}
-    }
+  /* Prepare the image data */
+  png_bytep *row_pointers; /* Image data */
+  int rp_height = data_size / rowbytes + 1; /* XXX special case needed */
   
-  if (amt < width * 3)
-    {
-      int i;
-      for (i = amt; i < width * 3; i++)
-	{
-	  row_pointers[data_size - 1][i+0] = 0;
-	  row_pointers[data_size - 1][i+1] = 0;
-	  row_pointers[data_size - 1][i+2] = 0;
-	}
-    }
-  if (amt == 0)
-    data_size--;
+  if (dat_info->png_height > 0 && 
+      rp_height > dat_info->png_height)
+    rp_height = dat_info->png_height;
   
-  FILE *fw = fopen("out.png", "wb");
+  row_pointers = (png_bytep *)
+    malloc(sizeof(png_bytep) * rp_height);
+  
+  FILE *fw = fopen(filename, "wb");
   
   /* Initialize png structs. */
-  png_structp pngw_ptr = png_create_write_struct
+  png_structp png_ptr = png_create_write_struct
     (PNG_LIBPNG_VER_STRING, NULL,
      NULL, NULL);
-  png_infop infow_ptr = png_create_info_struct(pngw_ptr);
-  png_init_io(pngw_ptr, fw);
+  png_infop info_ptr = png_create_info_struct(png_ptr);
+  png_init_io(png_ptr, fw);
   
-  /* Set image meta data. */
-  infow_ptr->width = width;
-  infow_ptr->height = data_size;
-  infow_ptr->valid = 0;
-  infow_ptr->rowbytes = width * 3;
-  infow_ptr->palette = NULL;
-  infow_ptr->num_palette = 0;
-  infow_ptr->num_trans = 0;
-  infow_ptr->bit_depth = 8;
-  infow_ptr->color_type = PNG_COLOR_TYPE_RGB;
-  infow_ptr->compression_type = PNG_COMPRESSION_TYPE_DEFAULT;
-  infow_ptr->filter_type = PNG_FILTER_TYPE_DEFAULT;
-  infow_ptr->interlace_type = PNG_INTERLACE_NONE;
-  
-  png_write_info(pngw_ptr, infow_ptr);
-  
-  if (setjmp(png_jmpbuf(pngw_ptr)))
-    printf("[write_png_file] Error during write\n");
+  /* Load data into row_pointers image data. */
+  int i;
+  int data_len;
+  for (i = 0; i < rp_height; i ++)
+    {
+      row_pointers[i] = (png_bytep) calloc(1, rowbytes);
+      
+      data_len = (i + 1) * rowbytes;
+      if (data_len > data_size)
+	data_len -= data_size;
+      else
+	data_len = rowbytes;
+      
+      /* 8-bit depth only: */ 
+      memcpy(row_pointers[i], data + i * rowbytes, data_len);
+    }
 
-  png_write_image(pngw_ptr, row_pointers);
-  png_write_end(pngw_ptr, NULL);
+  /* Set image meta data. */
+  info_ptr->width = dat_info->png_width;
+  info_ptr->height = rp_height;
+  info_ptr->valid = 0;
+  info_ptr->rowbytes = rowbytes;
+  info_ptr->palette = NULL;
+  info_ptr->num_palette = 0;
+  info_ptr->num_trans = 0;
+  info_ptr->bit_depth = dat_info->bit_depth;
+  info_ptr->color_type = PNG_COLOR_TYPE_RGB;
+  info_ptr->compression_type = PNG_COMPRESSION_TYPE_DEFAULT;
+  info_ptr->filter_type = PNG_FILTER_TYPE_DEFAULT;
+  info_ptr->interlace_type = PNG_INTERLACE_NONE;
   
-  printf("Done.\n");
+  png_write_info(png_ptr, info_ptr);
+  
+  if (setjmp(png_jmpbuf(png_ptr)))
+    printf("[write_png_file] Error during write\n");
+  
+  png_write_image(png_ptr, row_pointers);
+  png_write_end(png_ptr, NULL);
+  
   fclose(fw);
   
   return EXIT_SUCCESS;
